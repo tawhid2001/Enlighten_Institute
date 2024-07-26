7aa97550e7eb404f696173a19dd957194372c30b
tawhid2001 <tawhid.talal@gmail.com>
2024-07-26 19:03:42 -0400

    custom user serializer

--- Enlighten/settings.py
+++ Enlighten/settings.py
@@ -78,6 +78,10 @@ REST_AUTH_REGISTER_SERIALIZERS = {
     'REGISTER_SERIALIZER': 'accounts.serializers.CustomRegisterSerializer',
 }
 
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER': 'accounts.serializers.CustomUserSerializer',
+}
+
 # CORS_ORIGIN_WHITELIST = (
 #     "http://localhost:3000",
 #     "http://localhost:8000",
